c4983fd2c77857188d93d4f38089d1a54425fecc
qaacademy <qaacademy@gmail.com>
2022-03-25 13:55:39 -0300

    Refatoração EX5 - easy

--- src/easy/Exercicio05.java
+++ src/easy/Exercicio05.java
@@ -1,27 +1,29 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio05 {
 
-    public static void main(String[] args) {
+    public int soma(int primeiroNumero, int segundoNumero, int terceiroNumero) {
+        int soma;
+        soma = primeiroNumero + segundoNumero + terceiroNumero;
+        return soma;
+    }
 
-        double numero1, numero2, numero3;
-        double soma, subtracao, multiplicacao,media;
+    public int subtracao(int primeiroNumero, int segundoNumero, int terceiroNumero) {
+        int subtracao;
+        subtracao = primeiroNumero - segundoNumero - terceiroNumero;
+        return subtracao;
+    }
 
-        numero1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro número"));
-        numero2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo número"));
-        numero3 = Double.parseDouble(JOptionPane.showInputDialog("Digite o terceiro número"));
+    public int multiplicacao(int primeiroNumero, int segundoNumero, int terceiroNumero) {
+        int multiplicacao;
+        multiplicacao = primeiroNumero * segundoNumero * terceiroNumero;
+        return multiplicacao;
+    }
 
-        soma = numero1 + numero2 + numero3;
-        subtracao = numero1 - numero2 - numero3;
-        multiplicacao = numero1 * numero2 * numero3;
-        media = (numero1 + numero2 + numero3) / 3;
-
-        System.out.println("A soma dos números digitados é: " + soma);
-        System.out.println("A subtração dos números digitados é: " + subtracao);
-        System.out.println("A soma dos multiplicacao digitados é: " + multiplicacao);
-        System.out.println("A soma dos media digitados é: " + media);
+    public int media(int soma) {
+        int media;
+        media = soma / 3;
+        return media;
 
     }
 
