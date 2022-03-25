85139cf4012aa081f50e0fc45613cfdb8b504a29
qaacademy <qaacademy@gmail.com>
2022-03-25 13:57:12 -0300

    Refatoração EX6 - easy

--- src/easy/Exercicio06.java
+++ src/easy/Exercicio06.java
@@ -4,26 +4,18 @@ import javax.swing.JOptionPane;
 
 public class Exercicio06 {
 
-    public static void main(String[] args) {
-
-        double nota1, nota2, media;
-
-        nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota"));
-        nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota"));
-        media = (nota1 + nota2) / 2;
-
-        if (media > 5) {
-            System.out.println("Aprovado");
+    public String verificaAprovacao(double nota1, double nota2) {
+        double resultado;
+        resultado = (nota1 + nota2) / 2;
+        if (resultado > 5) {
+            return "Você está Aprovado! Parabéns!!!";
         }
-
-        if (media < 5) {
-            System.out.println("Reprovado");
+        if (resultado < 5) {
+            return ("Você está Reprovado!");
         }
-
-        if (media == 5) {
-            System.out.println("Exame");
+        if (resultado == 5) {
+            return ("Você está de Exame");
         }
-
+        return "Não foi possívl calcular sua nota";
     }
-
 }
