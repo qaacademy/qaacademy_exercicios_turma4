8cf912e4d355490753e84ebf302f082085ab20ff
qaacademy <qaacademy@gmail.com>
2022-03-25 13:52:58 -0300

    Refatoração Ex3 - easy

--- src/easy/Exercicio03.java
+++ src/easy/Exercicio03.java
@@ -4,18 +4,11 @@ import javax.swing.JOptionPane;
 
 public class Exercicio03 {
 
-    public static void main(String[] args) {
-        
-        int numero1;
-        int numero2; 
-        
-        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
-        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite um outro numero")); 
-        // Double.parseDouble(JOptionPane.showInputDialog("Digite um outro numero"));
-        
-        System.out.println("O valor do primeiro numero é :" + numero2);
-        System.out.println("O valor do segundo numero é :" + numero1);
+    public String retornaValorTrocado(String primeiroValor, String segundoValor) {
 
+        String msgNumerosTrocados =  "Primeiro valor digitado: " + segundoValor + "\n" +
+        "Segundo valor digitado: " + primeiroValor;
+        return
     }
-    
+
 }
