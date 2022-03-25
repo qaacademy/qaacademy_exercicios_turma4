3f924f4def4b166cd5ae43ee1dd3553b59db9f1d
qaacademy <qaacademy@gmail.com>
2022-03-25 13:58:44 -0300

    Refatoração EX07 - easy

--- src/easy/Exercicio07.java
+++ src/easy/Exercicio07.java
@@ -4,35 +4,25 @@ import javax.swing.JOptionPane;
 
 public class Exercicio07 {
 
-    public static void main(String[] args) {
-
-        double salario, inss;
-        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota"));
-
-        if(salario <= 1045.00){
-        inss = salario * 0.075;
-        System.out.println("O Valor que você de inss será: "+ inss);
+    public double calculaInss(double salario) {
+        double inss = 0.0; // declaração das variaveis
+        if (salario <= 1045.00) {// abertura
+            inss = salario * 0.075; // 7,5%
+        } // fechamento do if
+        if (salario >= 1045.01 && salario <= 2089.60) {
+            inss = salario * 0.09; // 9%
+        }
+        if (salario >= 2089.61 && salario <= 3134.40) {
+            inss = salario * 0.12;// 12%
         }
 
-        if(salario >= 1045.01 && salario<= 2089.60){
-        inss = salario * 0.09;
-        System.out.println("O valor de Inss será: "+ inss);
+        if (salario >= 3134.41 && salario <= 6101.06) {
+            inss = salario * 0.14; // 14%
         }
-
-        if(salario >= 2089.61 && salario<=3134.40){
-        inss = salario * 0.12;
-        System.out.println("O valor de Inss será: "+ inss);
+        if (salario > 6101.06) {
+            inss = 854.15; // Teto
         }
-
-        if(salario >= 3134.41 && salario <= 6101.06){
-        inss = salario * 0.14;
-        System.out.println("O Valor que você de inss será: "+ inss);
-        }
-
-        if(salario >= 6101.07){
-        inss = 854.15;
-        System.out.println("O Valor que você de inss será: "+ inss);
-        }
+        return inss;
 
     }
 
