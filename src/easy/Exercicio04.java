bff45b19861a97886961b19e9e7abd9f49c29517
qaacademy <qaacademy@gmail.com>
2022-03-25 13:54:03 -0300

    Refatoração EX4 easy

--- src/easy/Exercicio04.java
+++ src/easy/Exercicio04.java
@@ -4,10 +4,9 @@ import javax.swing.JOptionPane;
 
 public class Exercicio04 {
 
-    public static void main(String[] args) {
-
-        String numeroDigitadoString = JOptionPane.showInputDialog("Digite o número que voce deseja calcular o dobro");
-        int numero = Integer.parseInt(numeroDigitadoString);
-        System.out.println("O dobro do numero " + numero + " é: " + numero * 2);
+    public int calcularDobro(int numeroDigitado) {
+        int resultado;
+        resultado = numeroDigitado * 2;
+        return resultado;
     }
 }
