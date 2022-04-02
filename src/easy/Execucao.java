d188bd41bb583371e29e67f4c046bb1d22da57c9
qaacademy <qaacademy@gmail.com>
2022-04-02 14:08:22 -0300

    Refatoração EX06 - easy

--- src/easy/Execucao.java
+++ src/easy/Execucao.java
@@ -12,11 +12,18 @@ public class Execucao {
         // System.out.println("O dobro do numero enviado: "+ 10 +" Será:
         // "+exercicio04.calcularDobro(10));
 
-        Exercicio05 exercicio05 = new Exercicio05();
+        // Exercicio05 exercicio05 = new Exercicio05();
 
-        System.out.println("A soma dos números digitados é: " + exercicio05.calcularSoma(10, 20, 30));
-        System.out.println("A subtração dos números digitados é: " + exercicio05.calcularSubtracao(10, 20, 30));
-        System.out.println("A multiplicacao dos números digitados é: " + exercicio05.calcularMultiplicacao(10, 20, 30));
-        System.out.println("A media dos números digitados é: " + exercicio05.calcularMedia(exercicio05.calcularSoma(10, 20, 30)));
+        // System.out.println("A soma dos números digitados é: " +
+        // exercicio05.calcularSoma(10, 20, 30));
+        // System.out.println("A subtração dos números digitados é: " +
+        // exercicio05.calcularSubtracao(10, 20, 30));
+        // System.out.println("A multiplicacao dos números digitados é: " +
+        // exercicio05.calcularMultiplicacao(10, 20, 30));
+        // System.out.println("A media dos números digitados é: " +
+        // exercicio05.calcularMedia(10, 20, 30));
+
+        Exercicio06 exercicio06 = new Exercicio06();
+        System.out.println(exercicio06.verificarAprovacao(5, 5));
     }
 }
--- src/easy/Exercicio06.java
+++ src/easy/Exercicio06.java
@@ -4,18 +4,22 @@ import javax.swing.JOptionPane;
 
 public class Exercicio06 {
 
-    public String verificaAprovacao(double nota1, double nota2) {
-        double resultado;
-        resultado = (nota1 + nota2) / 2;
-        if (resultado > 5) {
-            return "Você está Aprovado! Parabéns!!!";
+    public String verificarAprovacao(double nota1, double nota2) {
+
+        double media;
+        media = (nota1 + nota2) / 2;
+
+        if (media > 5) {
+           return "Aprovado";
         }
-        if (resultado < 5) {
-            return ("Você está Reprovado!");
+        if (media < 5) {
+            return "Reprovado";
         }
-        if (resultado == 5) {
-            return ("Você está de Exame");
+        if (media == 5) {
+            return "Exame";
         }
-        return "Não foi possívl calcular sua nota";
+        return "Não foi possivel calcular sua media";
+
     }
+
 }
