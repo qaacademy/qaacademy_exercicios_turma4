ba40465b04951654074cd16c69d70ac638fd842e
qaacademy <qaacademy@gmail.com>
2022-04-09 10:47:11 -0300

    Refatoração EX5 - medium

--- src/medium/Execucao.java
+++ src/medium/Execucao.java
@@ -5,20 +5,24 @@ public class Execucao {
     public static void main(String[] args) {
         // Exercicio02 exercicio02 = new Exercicio02();
         // double valorInvestimento = 100000.00;
-        // double valorJuros = exercicio02.calcularInvestimentoJurosCompostos(valorInvestimento);
+        // double valorJuros =
+        // exercicio02.calcularInvestimentoJurosCompostos(valorInvestimento);
         // double valorTotal = valorInvestimento + valorJuros;
-        
+
         // System.out.println("O Valor investido rendeu : " + valorJuros + "de Juros");
         // System.out.println("Você investiu: " + valorInvestimento);
         // System.out.println("O Valor total são : " + valorTotal);
 
-        // Exercicio03 exercicio03 = new Exercicio03(); 
+        // Exercicio03 exercicio03 = new Exercicio03();
         // System.out.println(exercicio03.somaAteMil());
 
-        Exercicio04 exercicio04 = new Exercicio04(); 
+        // Exercicio04 exercicio04 = new Exercicio04();
 
-        exibirVetor(exercicio04.multiplicaAteMil());
+        // exibirVetor(exercicio04.multiplicaAteMil());
 
+        int[] vetorNumeros = { 5, 4, 8, 1, 2 }; // Cria o vetorEpreenche os valores
+        Exercicio05 exercicio05 = new Exercicio05();
+        System.out.println(exercicio05.verificaMenorValor(vetorNumeros));
 
     }
 
--- src/medium/Exercicio05.java
+++ src/medium/Exercicio05.java
@@ -4,15 +4,16 @@ import javax.swing.JOptionPane;
 
 public class Exercicio05 {
 
-    public static void main(String[] args) {
+    public int verificaMenorValor(int[] vetorNumeros) {
         int i = 1, menor = 0, numeroDigitado;
         while (i <= 5) {
-            numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+            numeroDigitado = vetorNumeros[i - 1];
             if (numeroDigitado < menor || i == 1) {
                 menor = numeroDigitado;
             }
             i++;
         }
-      System.out.println("O menor digitado é: "+menor);
+        return menor;
     }
+
 }
